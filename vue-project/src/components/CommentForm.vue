247cc77bf918b74207544af24eb7913763ea5a4d
Atasha Frances Gayle Doria <asdoria@student.apc.edu.ph>
2025-02-03 10:24:51 +0000

    ud

--- vue-project/src/components/CommentForm.vue
+++ vue-project/src/components/CommentForm.vue
@@ -53,30 +53,35 @@
   </script>
   
   <style scoped>
-  /* Basic styling - Customize as needed */
-  .form-group {
-    border: none;
-    margin-bottom: 1rem;
-    background-color: #DBD8D1;
-  }
-  
-  label {
-    display: block;
-    margin-bottom: 0.5rem;
-  }
-  
-  .form-control {
-    width: 90%;
-    padding: 0.5rem;
-  }
-  
-  .btn {
-    padding: 0.5rem 1rem;
-    background-color: #31589B;
-    font-family: "Poppins", serif;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-  </style>
+.form-group {
+  margin-bottom: 1rem;
+  background-color: #DBD8D1;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+.form-control {
+  width: 90%;
+  padding: 0.5rem;
+  border: 1px solid #ccc; 
+  outline: none; 
+}
+
+textarea.form-control { 
+    border: 1px solid #ccc; 
+    outline: none;
+}
+
+.btn {
+  padding: 0.5rem 1rem;
+  background-color: #31589B;
+  font-family: "Poppins", serif;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
